5e137bf530d8211b05968b7fb53097b9954d7554
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-02 23:51:20 -0300

    refactor: Refactor user_converter.go to handle error cases

--- Partus_users/internal/converters/user_converter.go
+++ Partus_users/internal/converters/user_converter.go
@@ -1,16 +1,37 @@
 package converters
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ToModelUser(user *api.User) *model.User {
-	objectId, _ := primitive.ObjectIDFromHex(user.Id)
+func ToModelUser(user *api.User) (*model.User, error) {
+	if user == nil {
+		return nil, errors.New("user não pode ser nil")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(user.Id)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao converter userId para ObjectID: %w", err)
+	}
+
+	modelPersonalInfo, err := ToModelPersonalInfo(user.Id, user.PersonalInfo)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao converter PersonalInfo para o modelo: %w", err)
+	}
+
+	modelAccountInfo, err := ToModelAccountInfo(user.Id, user.AccountInfo)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao converter AccountInfo para o modelo: %w", err)
+	}
+
 	return &model.User{
 		Id:           objectId,
-		PersonalInfo: ToModelPersonalInfo(user.PersonalInfo),
-		AccountInfo:  ToModelAccountInfo(user.AccountInfo),
-	}
+		PersonalInfo: *modelPersonalInfo,
+		AccountInfo:  *modelAccountInfo,
+	}, nil
 }
