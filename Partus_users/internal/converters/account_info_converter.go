82366ab8b610900b29c458f1562d406d9e95e26c
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-03 01:15:08 -0300

    refactor: Refactor ToModelAccountInfo function signature

--- Partus_users/internal/converters/account_info_converter.go
+++ Partus_users/internal/converters/account_info_converter.go
@@ -2,21 +2,15 @@ package converters
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ToModelAccountInfo(userId string, accountInfo *api.AccountInfo) (*model.AccountInfo, error) {
+func ToModelAccountInfo(objectId primitive.ObjectID, accountInfo *api.AccountInfo) (*model.AccountInfo, error) {
 	if accountInfo == nil {
 		return nil, errors.New("accountInfo não pode ser nil")
-	}
-
-	objectId, err := primitive.ObjectIDFromHex(userId)
-	if err != nil {
-		return nil, fmt.Errorf("falha ao converter userId para ObjectID: %w", err)
 	}
 
 	return &model.AccountInfo{
