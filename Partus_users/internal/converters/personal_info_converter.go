8f8901df690716be90d68e8b3eed086b0172ebb1
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-03 01:15:32 -0300

    refactor: Refactor ToModelPersonalInfo function signature

--- Partus_users/internal/converters/personal_info_converter.go
+++ Partus_users/internal/converters/personal_info_converter.go
@@ -2,21 +2,15 @@ package converters
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ToModelPersonalInfo(userId string, personalInfo *api.PersonalInfo) (*model.PersonalInfo, error) {
+func ToModelPersonalInfo(objectId primitive.ObjectID, personalInfo *api.PersonalInfo) (*model.PersonalInfo, error) {
 	if personalInfo == nil {
 		return nil, errors.New("personalInfo não pode ser nil")
-	}
-
-	objectId, err := primitive.ObjectIDFromHex(userId)
-	if err != nil {
-		return nil, fmt.Errorf("falha ao converter userId para ObjectID: %w", err)
 	}
 
 	return &model.PersonalInfo{
