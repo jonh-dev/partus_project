0c6a4dd5fe8008b1dcf71604f34314460eb0fa91
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-02 23:55:04 -0300

    feat: Add mock user repository implementation

---
+++ Partus_users/internal/tests/mocks/repositories/mock_repository_user.go
@@ -0,0 +1,36 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/jonh-dev/partus_users/internal/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) CreateUser(ctx context.Context, user *model.User) (*model.User, error) {
+	args := m.Called(ctx, user)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.User), args.Error(1)
+}
+
+func (m *MockUserRepository) GetUser(ctx context.Context, id string) (*model.User, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.User), args.Error(1)
+}
+
+func (m *MockUserRepository) UpdateUser(ctx context.Context, user *model.User) (*model.User, error) {
+	args := m.Called(ctx, user)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.User), args.Error(1)
+}
