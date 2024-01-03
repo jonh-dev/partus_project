c001d53cbb8dab23d8b04e4d989267e6ee53d301
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-02 23:50:54 -0300

    refactor: Refactor ToModelAccountInfo function to handle errors and validate input

--- Partus_users/internal/converters/account_info_converter.go
+++ Partus_users/internal/converters/account_info_converter.go
@@ -1,12 +1,26 @@
 package converters
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ToModelAccountInfo(accountInfo *api.AccountInfo) *model.AccountInfo {
+func ToModelAccountInfo(userId string, accountInfo *api.AccountInfo) (*model.AccountInfo, error) {
+	if accountInfo == nil {
+		return nil, errors.New("accountInfo não pode ser nil")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao converter userId para ObjectID: %w", err)
+	}
+
 	return &model.AccountInfo{
+		UserId:                objectId,
 		Username:              accountInfo.Username,
 		Password:              accountInfo.Password,
 		AccountStatus:         model.AccountStatus(accountInfo.AccountStatus),
@@ -19,5 +33,5 @@ func ToModelAccountInfo(accountInfo *api.AccountInfo) *model.AccountInfo {
 		LastFailedLoginReason: accountInfo.LastFailedLoginReason,
 		AccountLockedUntil:    accountInfo.AccountLockedUntil.AsTime(),
 		AccountLockedReason:   accountInfo.AccountLockedReason,
-	}
+	}, nil
 }
