d64be3a1fa25ea576b1943a5953575b1467c524b
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-02 23:51:07 -0300

    refactor: Refactor personal info converter to handle user ID conversion

--- Partus_users/internal/converters/personal_info_converter.go
+++ Partus_users/internal/converters/personal_info_converter.go
@@ -1,16 +1,31 @@
 package converters
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ToModelPersonalInfo(personalInfo *api.PersonalInfo) *model.PersonalInfo {
-	return &model.PersonalInfo{
-		FirstName: personalInfo.FirstName,
-		LastName:  personalInfo.LastName,
-		Email:     personalInfo.Email,
-		BirthDate: personalInfo.BirthDate.AsTime(),
-		Phone:     personalInfo.Phone,
+func ToModelPersonalInfo(userId string, personalInfo *api.PersonalInfo) (*model.PersonalInfo, error) {
+	if personalInfo == nil {
+		return nil, errors.New("personalInfo não pode ser nil")
 	}
+
+	objectId, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao converter userId para ObjectID: %w", err)
+	}
+
+	return &model.PersonalInfo{
+		UserId:       objectId,
+		FirstName:    personalInfo.FirstName,
+		LastName:     personalInfo.LastName,
+		Email:        personalInfo.Email,
+		BirthDate:    personalInfo.BirthDate.AsTime(),
+		Phone:        personalInfo.Phone,
+		ProfileImage: personalInfo.ProfileImage,
+	}, nil
 }
