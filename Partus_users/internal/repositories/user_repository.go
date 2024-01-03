d1d06da38b1ddc38397d690cabf784b04d91d8f3
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-02 23:52:46 -0300

    refactor: Refactor user_repository.go

--- Partus_users/internal/repositories/user_repository.go
+++ Partus_users/internal/repositories/user_repository.go
@@ -2,10 +2,9 @@ package repositories
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/jonh-dev/partus_users/api"
 	"github.com/jonh-dev/partus_users/internal/config"
-	"github.com/jonh-dev/partus_users/internal/converters"
 	"github.com/jonh-dev/partus_users/internal/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -13,11 +12,9 @@ import (
 )
 
 type IUserRepository interface {
-	CreateUser(ctx context.Context, personalInfo *api.PersonalInfo, accountInfo *api.AccountInfo) (*api.User, error)
-	UpdateUser(ctx context.Context, user *api.User) (*api.User, error)
-	GetUser(ctx context.Context, id string) (*api.User, error)
-	GetUserByEmail(ctx context.Context, email string) (*api.User, error)
-	GetUserByUsername(ctx context.Context, username string) (*api.User, error)
+	CreateUser(ctx context.Context, user *model.User) (*model.User, error)
+	GetUser(ctx context.Context, id string) (*model.User, error)
+	UpdateUser(ctx context.Context, user *model.User) (*model.User, error)
 }
 
 type UserRepository struct {
@@ -30,50 +27,18 @@ func NewUserRepository(dbService *config.DBService) IUserRepository {
 	}
 }
 
-func (r *UserRepository) CreateUser(ctx context.Context, personalInfo *api.PersonalInfo, accountInfo *api.AccountInfo) (*api.User, error) {
+func (r *UserRepository) CreateUser(ctx context.Context, user *model.User) (*model.User, error) {
 	collection := r.getCollection()
-
-	modelPersonalInfo := converters.ToModelPersonalInfo(personalInfo)
-	modelAccountInfo := converters.ToModelAccountInfo(accountInfo)
-
-	user := &model.User{
-		PersonalInfo: modelPersonalInfo,
-		AccountInfo:  modelAccountInfo,
-	}
 
 	_, err := collection.InsertOne(ctx, user)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("falha ao inserir usuário no banco de dados: %w", err)
 	}
 
-	return user.ToProto(), nil
+	return user, nil
 }
 
-func (r *UserRepository) UpdateUser(ctx context.Context, user *api.User) (*api.User, error) {
-	collection := r.getCollection()
-
-	objectId, err := primitive.ObjectIDFromHex(user.Id)
-	if err != nil {
-		return nil, err
-	}
-
-	modelUser := converters.ToModelUser(user)
-
-	filter := bson.M{"_id": objectId}
-	update := bson.M{"$set": bson.M{
-		"personalinfo": modelUser.PersonalInfo,
-		"accountinfo":  modelUser.AccountInfo,
-	}}
-
-	_, err = collection.UpdateOne(ctx, filter, update)
-	if err != nil {
-		return nil, err
-	}
-
-	return modelUser.ToProto(), nil
-}
-
-func (r *UserRepository) GetUser(ctx context.Context, id string) (*api.User, error) {
+func (r *UserRepository) GetUser(ctx context.Context, id string) (*model.User, error) {
 	collection := r.getCollection()
 
 	var user model.User
@@ -87,41 +52,28 @@ func (r *UserRepository) GetUser(ctx context.Context, id string) (*api.User, err
 		return nil, err
 	}
 
-	return user.ToProto(), nil
-}
-
-func (r *UserRepository) GetUserByEmail(ctx context.Context, email string) (*api.User, error) {
-	collection := r.getCollection()
-
-	var user model.User
-	filter := bson.M{"personalinfo.email": email}
-	err := collection.FindOne(ctx, filter).Decode(&user)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	return user.ToProto(), nil
-}
-
-func (r *UserRepository) GetUserByUsername(ctx context.Context, username string) (*api.User, error) {
-	collection := r.getCollection()
-
-	var user model.User
-	filter := bson.M{"accountinfo.username": username}
-	err := collection.FindOne(ctx, filter).Decode(&user)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	return user.ToProto(), nil
+	return &user, nil
 }
 
 func (r *UserRepository) getCollection() *mongo.Collection {
 	return r.dbService.Client.Database(r.dbService.DBName).Collection("users")
+}
+
+func (r *UserRepository) UpdateUser(ctx context.Context, user *model.User) (*model.User, error) {
+	collection := r.getCollection()
+
+	filter := bson.M{"_id": user.Id}
+	update := bson.M{
+		"$set": bson.M{
+			"personalInfo": user.PersonalInfo,
+			"accountInfo":  user.AccountInfo,
+		},
+	}
+
+	_, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return nil, fmt.Errorf("falha ao atualizar usuário no banco de dados: %w", err)
+	}
+
+	return user, nil
 }
